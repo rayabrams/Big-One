e840bf743a3ea91b8829325587a05d40fc2296ca
Ray <rayabrams@yahoo.com>
2011-01-21 17:04:03 -0500

    initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bigone::Application.config.secret_token = '91ea612b16a8941979eb35fa0c56ef80ff61a7eebdd459109dd969c691f4fe34ff83a00cf468e4aac133a79a27a6a4e75d685a4518fa11a7bda55d43eb9844e5'
